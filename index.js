8889d6596b9bcc28a77b5da9f7acf570aef930ac
Ian Gathui <ian.gathui@student.moringaschool.com>
2022-12-12 09:38:16 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    console.log(customerName);
+}
+
+function setBestCustomer(){
+    window.bestCustomer = "not bob";
+    console.log(bestCustomer);
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    console.log(bestCustomer);
+}
+
+const leastFavoriteCustomer = "ian";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "not ian";
+    console.log(leastFavoriteCustomer);
+}
